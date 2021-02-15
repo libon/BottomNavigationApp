f74ae08733cb6bbc4a7f8f7539cdfe2fb1fb6579
Carmen Alvarez <calvarez@libon.com>
2021-02-15 18:26:24 +0100

    Don't use the navigation components library for switching between tabs
    * Use our own "adapter" to show/hide fragments when tabs are clicked
    * Remove the navigation components library
    * Add the fragment library
    * Use last working library versions: activity 1.1.0, fragment 1.2.5

--- app/src/main/java/com/example/bottomnavigationapp/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationapp/MainActivity.kt
@@ -1,29 +1,27 @@
 package com.example.bottomnavigationapp
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.core.view.doOnNextLayout
+import com.example.bottomnavigationapp.ui.BottomNavigationAdapter
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private val bottomNavigationAdapter by lazy { BottomNavigationAdapter(supportFragmentManager) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        bottomNavigationAdapter.createAllTabs()
+        val navView = findViewById<BottomNavigationView>(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener {
+            if (it.itemId != navView.selectedItemId) {
+                bottomNavigationAdapter.openTab(it.itemId)
+            }
+            true
+        }
+        navView.doOnNextLayout {
+            bottomNavigationAdapter.openTab(R.id.navigation_home)
+        }
     }
 }
---
+++ app/src/main/java/com/example/bottomnavigationapp/ui/BottomNavigationAdapter.kt
@@ -0,0 +1,67 @@
+package com.example.bottomnavigationapp.ui
+
+import androidx.annotation.IdRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import com.example.bottomnavigationapp.R
+import com.example.bottomnavigationapp.ui.dashboard.DashboardFragment
+import com.example.bottomnavigationapp.ui.home.HomeFragment
+import com.example.bottomnavigationapp.ui.notifications.NotificationsFragment
+
+class BottomNavigationAdapter(private val fm: FragmentManager) {
+    companion object {
+        private val tabIds = listOf(
+            R.id.navigation_home,
+            R.id.navigation_dashboard,
+            R.id.navigation_notifications,
+        )
+    }
+
+    fun createAllTabs() {
+        val transaction = fm.beginTransaction()
+        tabIds.forEach { tabId ->
+            val fragment: Fragment = getItem(tabId) ?: (createFragment(tabId).also {
+                transaction.add(R.id.selected_tab_content, it, getTag(tabId))
+            })
+            transaction.setMaxLifecycle(fragment, Lifecycle.State.CREATED)
+            transaction.hide(fragment)
+            fragment.setMenuVisibility(false)
+        }
+        transaction.commitAllowingStateLoss()
+    }
+
+    fun openTab(@IdRes selectedTabId: Int) {
+        val transaction = fm.beginTransaction()
+        tabIds.forEach { tabId ->
+            fm.findFragmentByTag(getTag(tabId))?.let { fragment ->
+                if (tabId != selectedTabId) {
+                    transaction.hide(fragment)
+                    transaction.setMaxLifecycle(fragment, Lifecycle.State.STARTED)
+                    fragment.setMenuVisibility(false)
+                } else {
+                    transaction.show(fragment)
+                    transaction.setMaxLifecycle(fragment, Lifecycle.State.RESUMED)
+                    fragment.setMenuVisibility(true)
+                }
+            }
+        }
+        transaction.commitAllowingStateLoss()
+    }
+
+    private fun createFragment(@IdRes tabId: Int) =
+        when (tabId) {
+            R.id.navigation_home -> HomeFragment()
+            R.id.navigation_dashboard -> DashboardFragment()
+            else /*R.id.navigation_notifications */ -> NotificationsFragment()
+        }
+
+    private fun getItem(@IdRes tabId: Int): Fragment? = fm.findFragmentByTag(getTag(tabId))
+
+    private fun getTag(@IdRes tabId: Int) =
+        when (tabId) {
+            R.id.navigation_home -> "home"
+            R.id.navigation_dashboard -> "dashboard"
+            else /*R.id.navigation_notifications*/ -> "notifications"
+        }
+}
